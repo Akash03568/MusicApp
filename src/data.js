ffc5bc46071afe306f3d22cc43f33f29333da425
Akash03568 <102283231+Akash03568@users.noreply.github.com>
2022-12-17 06:39:28 +0530

    push

---
+++ src/data.js
@@ -0,0 +1,81 @@
+import { v4 as uuidv4 } from "uuid";
+
+function chillHop(){
+    return [
+        {
+            name: "Auliya",
+            cover: "https://drive.google.com/uc?export=download&id=1outCJes5pdJr8cZGGama-pc0wK8zJET6",
+            artist: "Simmo",
+            id: uuidv4(),
+            active: true,
+            color: ["#e9b09f", "#bc477c"],
+            audio: "https://drive.google.com/uc?export=download&id=1NApC92aUJ2qTVwTmPMZMWq_vjQLOMq6D",
+        },
+        {
+            name: "Afreen",
+            cover: "https://drive.google.com/uc?export=download&id=1txdiZXg-ADKDIvn7nbVlgqrYGeyny5ZQ",
+            artist: "Simmo",
+            id: uuidv4(),
+            active: false,
+            color: ["#c4c4c4", "#585858"],
+            audio: "https://drive.google.com/uc?export=download&id=1D6ra-Y-bgCZ2uFJgNL4KUHW4eTeBeh6_",
+        },
+        {
+            name: "Without Me",
+            cover: "https://drive.google.com/uc?export=download&id=1rLPhq9GkxtWxgfjJjNxAOGAwtcSmARsa",
+            artist: "Simmo ft. halsey",
+            id: uuidv4(),
+            active: false,
+            color:["#825e64", "#121132"],
+            audio: "https://drive.google.com/uc?export=download&id=1ajQOp3iL-Zbmai2PKwNnbqK8cioCa69m",
+        },
+        {
+            name: "Memba",
+            cover: "https://drive.google.com/uc?export=download&id=10JSPz7JgKDsfkw80GnJ1eN4VD3uOGTnM",
+            artist: "Simmo ft. NOORAN SISTERS",
+            id: uuidv4(),
+            active: false,
+            color: ["#ede4d3", "#9c8072"],
+            audio: "https://drive.google.com/uc?export=download&id=1v9DM0iwYTG5qD9XljIx5ukSlIVSBx_Qh",
+        },
+        {
+            name: "Tune Kaha",
+            cover: "https://drive.google.com/uc?export=download&id=1iAuSh3YcBz6ZwonRPfDCFFdwVWih5YEW",
+            artist: "Simmo ft. Prateek Kuhad",
+            id: uuidv4(),
+            active: false,
+            color:["#939393", "#292929"],
+            audio: "https://drive.google.com/uc?export=download&id=13EO7CK0-KjV-x46hbC0A-t9halIYPZLv",
+        },
+        
+        {
+            name: "Zara Zara",
+            cover: "https://drive.google.com/uc?export=download&id=1OCARbk8YVrA6tYyCu-MY5m0L24CtjiJW",
+            artist: "Simmo",
+            id: uuidv4(),
+            active: false,
+            color:["#e18f82", "#6b7894"],
+            audio: "https://drive.google.com/uc?export=download&id=1reujdnOcDVte3IE5eKUO--1ePf74xo8h",
+        },
+        {
+            name: "Toss a Coin",
+            cover: "https://drive.google.com/uc?export=download&id=1YxNtzHct1jAWbL_9RTgN-DXs11bJY6Ml",
+            artist: "Simmmmo",
+            id: uuidv4(),
+            active: false,
+            color:["#e188ca", "#cd2d21"],
+            audio: "https://drive.google.com/uc?export=download&id=1UaTqnBH8nb2iKBoXMAJm_J4lqs93fj6l",
+        },
+        {
+            name: "All I Want",
+            cover: "https://drive.google.com/uc?export=download&id=1Rs-SevWf69i_H_mt89YcEdjKYk_4ZuTf",
+            artist: "Kodaline",
+            id: uuidv4(),
+            active: false,
+            color: ["#592f16", "#251b1a"],
+            audio: "https://drive.google.com/uc?export=download&id=1pbMiulAudok6FL4QkvGOwN1MwHh71jKD",
+        },
+    ]
+}
+
+export default chillHop;
